628fbbf33225a09a10fed834797dc1986df401f0
Angelo Sotomayor <assv.9330@gmail.com>
2022-03-08 18:25:59 -0600

    Inicialización del proyecto

---
+++ src/main/java/com/IExamen/IExamenApplication.java
@@ -0,0 +1,13 @@
+package com.IExamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IExamenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/IExamen/IExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IExamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
